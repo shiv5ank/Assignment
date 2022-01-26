035fcb477d83ddff3e9175944690fbd9a5cca3e4
shiv5ank <90241832+shiv5ank@users.noreply.github.com>
2022-01-26 16:36:40 +0530

    25Jan-Assignment

---
+++ Assignment-25Jan/Que2.js
@@ -0,0 +1,13 @@
+var num1 = 50;
+var num2 = 30;
+var num3 = 10;
+
+if (num1>num2 && num1>num3) {
+   console.log("num1 is greater");
+}
+else if (num2>num1 && num2>num3) {
+   console.log("num2 is greater");
+}
+else {
+   console.log("num3 is greater");
+}
